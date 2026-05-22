a87159cbffaad8c9da9086ba00f12e6890c24b6d
Jovanni Feliz <feliz.jovanni@gmail.com>
2026-05-21 20:43:41 -0500

    Fixed environment variables

--- backend/ironforge/settings.py
+++ backend/ironforge/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'ironforge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('ALLOWED_HOSTS'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
